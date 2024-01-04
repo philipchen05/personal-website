c046c767d4f5378f62c68d56cb899956fa07e530
Philip Chen <philipchen@philips-air.home>
2024-01-04 05:49:38 -0500

    Deployed website

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 function Subtitle(props) {
+    var divClass;
+    var h1Class;
+
     if(props.fixed) {
-        return(
-            <div data-aos="fade-right" data-aos-duration="2000" className="pr-20 pt-40">
-                <h1 className="md:fixed lg:fixed text-base md:text-lg lg:text-xl font-body text-white tracking-widest">COMPUTER SCIENCE @UWATERLOO</h1>
-            </div>
-        );
+        divClass = "pr-20 pt-40";
+        h1Class = "md:fixed lg:fixed text-base md:text-lg lg:text-xl font-body text-white tracking-widest";
     } else {
-        return(
-            <div data-aos="fade-right" data-aos-duration="2000" className="pt-5">
-                <h1 className="text-base md:text-lg lg:text-xl font-body text-white tracking-widest">COMPUTER SCIENCE @UWATERLOO</h1>
-            </div>
-        );
-    }
+        divClass = "pt-5";
+        h1Class = "ext-base md:text-lg lg:text-xl font-body text-white tracking-widest"}
+    return(
+        <div data-aos="fade-right" data-aos-duration="2000" className={divClass}>
+            <h1 className={h1Class}>COMPUTER SCIENCE @UWATERLOO</h1>
+        </div>
+    );
 }
 
 export default Subtitle;
