2b61a9025f9801c9a05679b00b4450d2b7f56ace
philipchen05 <140920198+philipchen05@users.noreply.github.com>
2024-01-10 17:35:11 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,8 +5,8 @@ function Projects() {
     return(
         <div className="animate-in fade-in-5 slide-in-from-bottom-5 duration-1000 flex flex-col gap-10">
             <Card url="https://github.com/philipchen05/zombie-survival" name="ZOMBIE SURVIVAL GAME" tools="Java, Swing" description="A top-down zombie survival video game created in Java. The game features a dynamic splash screen, a main menu, a live leaderboard, and a game over screen." />
-            <Card url="https://github.com/philipchen05/mark-book" name="MARK BOOK PROGRAM" tools="Java" description="A mark book program created using Java for teachers to store, view, and update student mark information." />
             <Card url="https://github.com/philipchen05/iss-tracker" name="ISS TRACKER" tools="Node.js, React, JavaScript, Axios, Tailwind CSS" description="A live International Space Station (ISS) tracker created with React that displays the location of the ISS on a world map." />
+            <Card url="https://github.com/philipchen05/mark-book" name="MARK BOOK PROGRAM" tools="Java" description="A mark book program created using Java for teachers to store, view, and update student mark information." />
         </div>
     );
 }
