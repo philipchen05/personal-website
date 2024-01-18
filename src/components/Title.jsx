38af1a66c2d82cc42e4239b5e1b751ea8efdc0e2
Philip <140920198+philipchen05@users.noreply.github.com>
2024-01-18 13:06:27 -0500

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Title() {
     return(
-        <div data-aos="fade-right" data-aos-delay="400" data-aos-duration="3000" className="pt-36">
+        <div className="animate-in fade-in slide-in-from-left duration-3400 pt-36">
             <h1 className="font-display text-white font-extralight text-7xl md:text-8xl lg:text-9xl">Philip<br />Chen</h1>
         </div>
     );
