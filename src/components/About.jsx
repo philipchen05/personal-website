8e62eb72a54ff00da9e27a4fa75f17c401eb7faf
Philip <140920198+philipchen05@users.noreply.github.com>
2024-01-24 20:20:51 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,8 @@ function About() {
     return(
         <div className="animate-in fade-in-5 slide-in-from-bottom-5 duration-1000">
             <h1 className="font-body text-xl md:text-2xl lg:text-2xl text-white font-thin">
-            As a lover of math and computer science, my dream is to work among the tech giants one day to
-            improve society through algorithms and computational innovation. I'm also very interested in
-            software development, machine learning, and AI.
+            As a lover of math and computer science, my dream is to improve society through algorithms and
+            computational innovation. I'm also very interested in software development, machine learning, and AI.
             </h1>
             <br />
             <h1 className="font-body text-xl md:text-2xl lg:text-2xl text-white font-thin">
