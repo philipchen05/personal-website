38ba856bd291a9463e76e8099c63a33d3ce6fc81
Philip <140920198+philipchen05@users.noreply.github.com>
2024-02-18 20:52:32 -0500

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 function Title() {
     return(
-        <div className="animate-in fade-in slide-in-from-left duration-3400 pt-36">
-            <button onClick={() => window.open("https://philipchen.ca/", "_self")}>
-                <h1 className="font-display text-white font-extralight text-7xl md:text-8xl lg:text-9xl">Philip<br />Chen</h1>
-            <button/>
-        </div>
+        <button className="animate-in fade-in slide-in-from-left duration-3400 pt-36" onClick={() => window.open("https://philipchen.ca/", "_self")}>
+            <h1 className="font-display text-white font-extralight text-7xl md:text-8xl lg:text-9xl">Philip<br />Chen</h1>
+        <button/>
     );
 }
 
