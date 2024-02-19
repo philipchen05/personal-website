71418cb17a4a33746c6145694937184411d80769
Philip <140920198+philipchen05@users.noreply.github.com>
2024-02-18 20:54:00 -0500

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ function Title() {
     return(
         <button className="animate-in fade-in slide-in-from-left duration-3400 pt-36" onClick={() => window.open("https://philipchen.ca/", "_self")}>
             <h1 className="font-display text-white font-extralight text-7xl md:text-8xl lg:text-9xl">Philip<br />Chen</h1>
-        <button/>
+        </button>
     );
 }
 
