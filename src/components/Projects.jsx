16c602a4e51b454df62d2d2429d67fcfec19f198
Philip <140920198+philipchen05@users.noreply.github.com>
2024-02-27 00:56:23 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,8 @@ import Card from "./Card";
 function Projects() {
     return(
         <div className="animate-in fade-in-5 slide-in-from-bottom-5 duration-1000 flex flex-col gap-10">
-            <Card url="https://github.com/philipchen05/zombie-survival" name="ZOMBIE SURVIVAL" tools="Java, Swing" description="A top-down zombie survival video game created in Java. The game features a dynamic splash screen, a main menu, a live leaderboard, and a game over screen." />
             <Card url="https://github.com/philipchen05/iss-tracker" name="ISS TRACKER" tools="Node.js, React, JavaScript, Axios, Tailwind" description="A live International Space Station (ISS) tracker created with React that displays the location of the ISS on a world map. Accessible at www.whereistheiss.io." />
+            <Card url="https://github.com/philipchen05/zombie-survival" name="ZOMBIE SURVIVAL" tools="Java, Swing" description="A top-down zombie survival video game created in Java. The game features a dynamic splash screen, a main menu, a live leaderboard, and a game over screen." />
             <Card url="https://github.com/kevinyvv/memoir" name="MEMOIR" tools="Python, Flask, MongoDB, React, JavaScript, Tailwind" description="A full-stack social media app for sharing and connecting with others through nostalgic memories (UofTHacks 11)." />
             <Card url="https://github.com/philipchen05/mark-book" name="TDSB Markbook" tools="Java" description="A mark book program created using Java for teachers to store, view, and update student mark information." />
         </div>
