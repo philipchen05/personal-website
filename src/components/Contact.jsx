70670e82b65f928bdf76d485dadcf8241893220e
Philip <140920198+philipchen05@users.noreply.github.com>
2024-05-26 17:18:06 -0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,11 +7,11 @@ function Contact() {
     return(
         <div className="animate-in fade-in-5 slide-in-from-bottom-5 duration-1000">
             <div className="flex flex-row justify-evenly">
-                <button><img src={linkedin} className="transition-1 duration-700 hover:scale-110 rounded-xl size-14 md:size-16 lg:size-18" alt="LinkedIn Logo." onClick={() => window.open("https://www.linkedin.com/in/philip-chen-489299232/", "_blank")} /></button>
-                <button><img src={instagram} className="transition-1 duration-700 hover:scale-110 size-14 md:size-16 lg:size-18" alt="Instagram Logo." onClick={() => window.open("https://www.instagram.com/philip__chen/", "_blank")} /></button>
-                <button><img src={github} className="transition-1 duration-700 hover:scale-110 rounded-xl size-14 md:size-16 lg:size-18" alt="GitHub Logo." onClick={() => window.open("https://github.com/philipchen05", "_blank")} /></button>
+                <button><img src={linkedin} className="transition-1 duration-700 hover:scale-110 rounded-xl xxs:size-10 xs:size-12 md:size-16 lg:size-18" alt="LinkedIn Logo." onClick={() => window.open("https://www.linkedin.com/in/philip-chen-489299232/", "_blank")} /></button>
+                <button><img src={instagram} className="transition-1 duration-700 hover:scale-110 xxs:size-10 xs:size-12 md:size-16 lg:size-18" alt="Instagram Logo." onClick={() => window.open("https://www.instagram.com/philip__chen/", "_blank")} /></button>
+                <button><img src={github} className="transition-1 duration-700 hover:scale-110 rounded-xl xxs:size-10 xs:size-12 md:size-16 lg:size-18" alt="GitHub Logo." onClick={() => window.open("https://github.com/philipchen05", "_blank")} /></button>
             </div>
-            <h1 className="leading-5 text-center font-body text-xl md:text-2xl lg:text-2xl text-white font-thin">
+            <h1 className="xs:mt-4 leading-5 text-center font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">
             <br />
             EMAIL: philipchen935@gmail.com
             <br />
