e2e963765a9a0c48af3601e1652078da8d192241
Philip <140920198+philipchen05@users.noreply.github.com>
2024-05-26 17:18:41 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Home(props) {
-    return <h1 className="font-body text-xl md:text-2xl lg:text-2xl text-white font-thin">Hello, my name is Philip Chen and I am a Computer Science student at the University of Waterloo.</h1>;
+function Home() {
+    return <h1 className="font-body xs:text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">Hello, my name is Philip Chen and I am a first year Computer Science student at the University of Waterloo.</h1>;
 }
 
 export default Home;
