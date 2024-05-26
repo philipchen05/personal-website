4be51e01ff82f7175e3da267bb444fd408f21d3d
Philip <140920198+philipchen05@users.noreply.github.com>
2024-05-26 17:17:24 -0400

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ function Title() {
     return(
         <div className="animate-in fade-in slide-in-from-left duration-3400 pt-36">
             <button onClick={() => window.open("https://philipchen.ca/", "_self")}>
-                <h1 className="font-display text-left text-white font-extralight text-6xl md:text-8xl lg:text-9xl">Philip<br />Chen</h1>
+                <h1 className="font-display text-left text-white font-extralight xxs:text-5xl xs:text-6xl sm:text-6xl md:text-8xl lg:text-9xl">Philip<br />Chen</h1>
             </button>
         </div>
     );
