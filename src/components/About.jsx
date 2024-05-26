c7f559d6329edf63c89de8a9f7e6eb923fd5e6dd
Philip <140920198+philipchen05@users.noreply.github.com>
2024-05-26 17:18:28 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,18 +3,18 @@ import React from "react";
 function About() {
     return(
         <div className="animate-in fade-in-5 slide-in-from-bottom-5 duration-1000">
-            <h1 className="font-body text-xl md:text-2xl lg:text-2xl text-white font-thin">
+            <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">
             As a lover of math and computer science, my dream is to improve society through algorithms and
             computational innovation. I'm also very interested in software development, machine learning, and AI.
             </h1>
             <br />
-            <h1 className="font-body text-xl md:text-2xl lg:text-2xl text-white font-thin">
+            <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">
             Outside of tech, I'm a very avid language learner. I'm fluent in English, Mandarin, and French —
             I am currently working on mastering Spanish. One of my life goals is to become a polyglot and be
             able to communicate with people all over the world.
             </h1>
             <br />
-            <h1 className="font-body text-xl md:text-2xl lg:text-2xl text-white font-thin">
+            <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">
             In my spare time, I enjoy listening to music, going to the gym, and kicking back to classic mobster films.
             </h1>
         </div>
