b7f0c640fdf059ffd2043a129a8cd36e532bd777
Philip <140920198+philipchen05@users.noreply.github.com>
2024-05-26 17:18:16 -0400

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Card(props) {
     return(
         <button onClick={() => {if(props.url!=null){window.open(props.url, "_blank")}}} className="text-left transition-1 duration-700 hover:scale-105 p-2 border-l-[1px] border-white">
-            <h1 className="font-body text-xl md:text-2xl lg:text-2xl text-white">{props.name + " "}<span className="italic font-thin text-[18px] md:text-[20px] lg:text-[20px]">{props.tools}</span></h1>
-            <h1 className="font-body text-xl md:text-2xl lg:text-2xl text-white font-thin">
+            <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white">{props.name + " "}<span className="italic font-thin text-[18px] md:text-[20px] lg:text-[20px]">{props.tools}</span></h1>
+            <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">
                 {props.description}
             </h1>
         </button>
