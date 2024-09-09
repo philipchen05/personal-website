eccb6ee435a4280a834bd950fbe7c9e248f563a9
Philip <philipchen935@gmail.com>
2024-09-09 18:49:19 -0400

    Updated website with new photo, resume, and projects

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card(props) {
     return(
         <button onClick={() => {if(props.url!=null){window.open(props.url, "_blank")}}} className="text-left transition-1 duration-700 hover:scale-105 p-2 border-l-[1px] border-white">
-            <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white">{props.name + " "}<span className="italic font-thin text-[16px] sm:text-[18px] md:text-[20px] lg:text-[20px]">{props.tools}</span></h1>
+            <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white">{props.name + " "}<br></br><span className="italic font-thin text-[14px] sm:text-[16px] md:text-[18px] lg:text-[18px]">{props.tools}</span></h1>
             <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">
                 {props.description}
             </h1>
