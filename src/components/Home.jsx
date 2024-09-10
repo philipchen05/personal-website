2a40e23a5d406a78c8a2dadd169e98a45f316882
Philip <philipchen935@gmail.com>
2024-09-10 11:56:27 -0400

    Updated home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home() {
-    return <h1 className="font-body xs:text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">Hello, my name is Philip and I study Computer Science at the University of Waterloo.</h1>;
+    return <h1 className="font-body xs:text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">My name is Philip and I study Computer Science at the University of Waterloo.</h1>;
 }
 
 export default Home;
