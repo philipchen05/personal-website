a3f84ed0b68bf743c64f2813a88abc059846dc34
Philip <philipchen935@gmail.com>
2025-02-16 19:32:48 -0500

    Changed About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,11 @@ function About() {
         <div className="animate-in fade-in-5 slide-in-from-bottom-5 duration-1000">
             <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">
             As a lover of math and computer science, my dream is to improve society through algorithms and
-            computational innovation. I'm also very interested in software development, machine learning, and AI.
+            computational innovation. I'm interested in software development, machine learning, and AI.
             </h1>
             <br />
             <h1 className="font-body text-lg sm:text-xl md:text-2xl lg:text-2xl text-white font-thin">
-            Outside of tech, I'm a very avid language learner. I'm fluent in English, Mandarin, and French —
+            Outside of tech, I'm an avid language learner. I'm fluent in English, Mandarin, and French —
             I am currently working on mastering Spanish. One of my life goals is to become a polyglot and be
             able to communicate with people all over the world.
             </h1>
